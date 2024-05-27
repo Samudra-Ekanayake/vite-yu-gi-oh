f5f226acf8a245188f986b83a36e3c83efeac86e
Samudra-Ekanayake <samudra2724@gmail.com>
2024-05-27 15:53:44 +0200

    Set up

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    AppHeader, Main, Footer,
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted() {
+  },
+}
+
+
+</script>
+
+<template>
+  
+</template>
+
+<style >
+
+</style>
