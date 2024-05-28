7164833ae3be1db28709f12d876c8dfb2b04559d
Samudra-Ekanayake <samudra2724@gmail.com>
2024-05-28 22:31:40 +0200

    chiamata al server

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,87 @@
+
+
 <script>
 import AppHeader from './components/AppHeader.vue'
 import Main from './components/Main.vue'
-import Footer from './components/Footer.vue'
+import CardList from './components/CardList.vue'
+import axios from 'axios'
 
 export default {
+
   components: {
-    AppHeader, Main, Footer,
+    AppHeader, Main, CardList,
   },
+  
+ 
+
   data() {
     return {
+      carte: []
     }
   },
+
+
   methods: {
   },
+
+
   mounted() {
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0").then((response) => {
+      console.log(response.data.data);
+      this.carte = response.data.data;
+    }).catch((error) => {
+      console.log(error)
+    });
   },
 }
 </script>
 
 <template>
-
   <AppHeader />
-
+  <Main :carte="carte"/>
+  <CardList  />
 </template>
 
 <style></style>
+
+
+
+<!--
+<template>
+  <AppHeader />
+  <Main :cards="cards" />
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import Main from './components/Main.vue'
+import CardList from './components/CardList.vue'
+import axios from 'axios'
+
+export default {
+  components: {
+    AppHeader, Main, CardList,
+  },
+  
+  data() {
+    return {
+      cards: []
+    }
+  },
+
+  mounted() {
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0").then((response) => {
+      console.log(response.data.data);
+      this.cards = response.data.data;
+    }).catch((error) => {
+      console.log(error)
+    });
+  },
+}
+</script>
+
+<style>
+</style>
+
+-->
+
